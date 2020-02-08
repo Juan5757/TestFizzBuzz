8f4772a901b931c3d2f1aebbf3244afdc411ee64
Juan5757 <52605634+Juan5757@users.noreply.github.com>
2020-02-08 15:41:29 -0400

    First Refactoring - Unit Tests and Everything is on main

--- TestFizzBuzz/src/main/Program.java
+++ TestFizzBuzz/src/main/Program.java
@@ -3,21 +3,7 @@ public class Program {
 	public static void main(String[] args)
 	{
 		
-		FizzBuzz fb = new FizzBuzz();
-		//fb.generar();
-		
-		int num = fb.generarUno(1);
-		System.out.println(num);
-		
-		String resultado = fb.generarFizz(3);
-		System.out.println(resultado);
-		
-		String resultado2 = fb.generarBuzz(5);
-		System.out.println(resultado2);
-		
-		String resultado3 = fb.generarFizzBuzz(15);
-		System.out.println(resultado3);
-		
+		FizzBuzz fb = new FizzBuzz();		
 		fb.generarMuchos(10);
     }	
 }
--- TestFizzBuzz/src/test/FizzBuzzTest.java
+++ TestFizzBuzz/src/test/FizzBuzzTest.java
@@ -1,117 +1,110 @@
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 	
+	private FizzBuzz fb;
+	
+	@Before
+	public void setUp(){
+		fb = new FizzBuzz();
+	}
+	
 	@Test
 	public void deberiaRetornar1SiIngreso1(){
-		FizzBuzz fb = new FizzBuzz();
 		int numero=fb.generarUno(1);
 		assertThat(1).isEqualTo(numero);
 	}
 	
 	@Test
 	public void deberiaRetornar2SiIngreso2(){
-		FizzBuzz fb = new FizzBuzz();
 		int numero=fb.generarUno(2);
 		assertThat(2).isEqualTo(numero);
 	}
 	
 	@Test
 	public void deberiaRetornarMenos1SiIngresoMenos1(){
-		FizzBuzz fb = new FizzBuzz();
 		int numero=fb.generarUno(-1);
 		assertThat(-1).isEqualTo(numero);
 	}
 	
 	@Test
 	public void deberiaRetornarMenos2SiIngresoMenos2(){
-		FizzBuzz fb = new FizzBuzz();
 		int numero=fb.generarUno(-2);
 		assertThat(-2).isEqualTo(numero);
 	}
 	
 	@Test
 	public void deberiaRetornarFizzSiIngreso3(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarFizz(3);
 		assertThat("Fizz").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarFizzSiIngreso6(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarFizz(6);
 		assertThat("Fizz").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornar4SiIngreso4(){
-		FizzBuzz fb = new FizzBuzz();
 		String resultado =fb.generarFizz(4);
 		assertThat("4").isEqualTo(resultado);
 	}
 	
 	@Test
 	public void deberiaRetornarBuzzSiIngreso5(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarBuzz(5);
 		assertThat("Buzz").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarBuzzSiIngreso10(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarBuzz(10);
 		assertThat("Buzz").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarBuzzSiIngreso15(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarBuzz(15);
 		assertThat("Buzz").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarBuzzSiIngreso12(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarBuzz(12);
 		assertThat("12").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarFizzBuzzSiIngreso15(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarFizzBuzz(15);
 		assertThat("FizzBuzz").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarFizzBuzzSiIngreso30(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarFizzBuzz(30);
 		assertThat("FizzBuzz").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarFizzBuzzSiIngreso10(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarFizzBuzz(10);
 		assertThat("10").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornarFizzBuzzSiIngreso11(){
-		FizzBuzz fb = new FizzBuzz();
 		String palabra =fb.generarFizzBuzz(11);
 		assertThat("11").isEqualTo(palabra);
 	}
 	
 	@Test
 	public void deberiaRetornar10NumerosAplicandoFizzBuzzSiIngreso10(){
-		FizzBuzz fb = new FizzBuzz();
 		fb.generarMuchos(10);
 	}
 	
